c31484559b2f96fed4aa2e5cd7f774501c32c551
arqrturo-gif <arqrturo@gmail.com>
2026-02-11 23:13:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,41 @@
-console.log("Eco Rana funcionando");
+// Estado del juego
+let score = 0;
+let yaClasificado = false;
+const categoriaCorrecta = "reciclable";
 
+// Referencias DOM
+const objeto = document.getElementById("objeto");
+const contenedores = document.querySelectorAll(".contenedor");
+const scoreEl = document.getElementById("score");
+
+// Drag
+objeto.addEventListener("dragstart", (e) => {
+  e.dataTransfer.setData("text/plain", "objeto");
+});
+
+// Drop
+contenedores.forEach(contenedor => {
+
+  contenedor.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+
+  contenedor.addEventListener("drop", (e) => {
+    e.preventDefault();
+
+    if (yaClasificado) return;
+
+    const categoriaDrop = contenedor.dataset.categoria;
+
+    if (categoriaDrop === categoriaCorrecta) {
+      score += 8;
+      alert("Muy bien!!!");
+    } else {
+      score -= 8;
+      alert("Error");
+    }
+
+    scoreEl.textContent = `Puntaje: ${score}`;
+    yaClasificado = true;
+  });
+});
