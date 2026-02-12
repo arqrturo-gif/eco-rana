cd98e2f2a6e6b5bff191906b77e1d065ed4a7c29
arqrturo-gif <arqrturo@gmail.com>
2026-02-12 00:00:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,75 +1,31 @@
-// ================================
-// DEFINICIÓN DE OBJETOS DEL JUEGO
-// ================================
-const objetosJuego = [
-  {
-    id: "obj3",
-    nombre: "Botella de gaseosa",
-    categoria: "reciclable",
-    imagen: "img/botella.png"
-  }
-];
-
-// ================================
-// ESTADO DEL JUEGO
-// ================================
-let score = 0;
 let yaClasificado = false;
 
-// ================================
-// REFERENCIAS DOM
-// ================================
-const scoreEl = document.getElementById("score");
-const zonaObjeto = document.getElementById("zona-objeto");
-const contenedores = document.querySelectorAll(".contenedor");
+const objeto = document.getElementById("objeto");
+const canecas = document.querySelectorAll(".caneca");
 
-// ================================
-// CREAR OBJETO VISUAL
-// ================================
-const objetoActivo = objetosJuego[0];
-
-const img = document.createElement("img");
-img.id = objetoActivo.id;
-img.src = objetoActivo.imagen;
-img.alt = objetoActivo.nombre;
-img.draggable = true;
-img.dataset.categoria = objetoActivo.categoria;
-
-zonaObjeto.appendChild(img);
-
-// ================================
-// DRAG
-// ================================
-img.addEventListener("dragstart", (e) => {
-  e.dataTransfer.setData("categoria", img.dataset.categoria);
+objeto.addEventListener("dragstart", e => {
+  e.dataTransfer.setData("categoria", objeto.dataset.categoria);
 });
 
-// ================================
-// DROP
-// ================================
-contenedores.forEach(contenedor => {
+canecas.forEach(caneca => {
 
-  contenedor.addEventListener("dragover", (e) => {
+  caneca.addEventListener("dragover", e => {
     e.preventDefault();
   });
 
-  contenedor.addEventListener("drop", (e) => {
+  caneca.addEventListener("drop", e => {
     e.preventDefault();
-
     if (yaClasificado) return;
 
     const categoriaObjeto = e.dataTransfer.getData("categoria");
-    const categoriaContenedor = contenedor.dataset.categoria;
+    const categoriaCaneca = caneca.dataset.categoria;
 
-    if (categoriaObjeto === categoriaContenedor) {
-      score += 8;
+    if (categoriaObjeto === categoriaCaneca) {
       alert("Muy bien!!!");
     } else {
-      score -= 8;
       alert("Error");
     }
 
-    scoreEl.textContent = `Puntaje: ${score}`;
     yaClasificado = true;
   });
 });
